501f4c14fd88d6629d87f56b4b76a883fe657cfd
DESKTOP-S0OAMUC\northka.chen <northka.chen@gmail.com>
2016-11-30 15:25:05 +0800

    http error 基础完成

--- custom/httpError/index.js
+++ custom/httpError/index.js
@@ -1,3 +1,43 @@
 /**
  * Created by northka.chen on 2016/11/29.
  */
+const httpStatusCodes = require('http').STATUS_CODES
+const util            = require('util')
+
+const httpError       = require('../../base/httpError')
+const httpErrorList   = require('./errorList')
+/**
+ * cutom httpError
+ * @param {Number}        statusCode
+ * @param {String|Object} messageOrProperties
+ * @param {Object}        properties
+ */
+function HttpErrors(statusCode, messageOrProperties, properties){
+    if(!(this instanceof httpErrors)){
+        return new HttpErrors(statusCode, messageOrProperties, properties)
+    }
+    statusCode = statusCode || 500
+    properties = properties || {}
+    if(messageOrProperties instanceof Object){
+        properties = messageOrPropertiess
+        let message
+        if(properties.message){
+            message = properties.message
+        }else{
+            if(httpErrorList[statusCode]){
+                message = httpErrorList[statusCode]
+            }else{
+                message = httpErrorList['500']
+            }
+        }
+        HttpErrors.super_.call(this, message, this.constructor)
+    }else if(typeof messageOrProperties === 'string'){
+        HttpErrors.super_.call(this, messageOrProperties, this.constructor)
+    }
+    Object.assign(this, properties)
+    this.name = httpStatusCodes[statusCode] || statusCode + 'Error'
+    this.statusCode = statusCode
+    this.status     = statusCode
+}
+util.inherits(HttpErrors, httpError)
+module.exports = HttpErrors
